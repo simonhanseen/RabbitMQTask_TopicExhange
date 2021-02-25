3168486b3240a1ea93ff39cd7193c42293a44ad6
simonhanseen <47739651+simonhanseen@users.noreply.github.com>
2021-02-25 14:41:04 +0100

    Add project files.

---
+++ RabbitMQTask_TopicExhange_Web/Pages/Index.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RabbitMQTask_TopicExhange_Web.RabbitMQ;
+
+namespace RabbitMQTask_TopicExhange_Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            var name = Request.Form["name"];
+            var email = Request.Form["email"];
+            var bookOrCancel = Request.Form["bookorcancel"];
+
+            List<string> message = new List<string>();
+
+            message.Add(bookOrCancel);
+            message.Add($"Name: {name}, Email: {email}");
+
+            Publish.PublishMessage(message.ToArray());
+
+        }
+
+
+    }
+}
---
+++ RabbitMQTask_TopicExhange_Web/RabbitMQ/Publish.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQTask_TopicExhange_Web.RabbitMQ
+{
+    public static class Publish
+    {
+        public static void PublishMessage(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "topic_tour",
+                                        type: "topic");
+
+                var routingKey = (args.Length > 0) ? args[0] : "anonymous.info";
+                var message = (args.Length > 1)
+                              ? string.Join(" ", args.Skip(1).ToArray())
+                              : "No message";
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "topic_tour",
+                                     routingKey: routingKey,
+                                     basicProperties: null,
+                                     body: body);
+            }
+        }
+    }
+}
